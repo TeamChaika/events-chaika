b197820bbe34bffc7d7310caef564d2299a991cb
TeamChaika <1@chaika.team>
2026-09-21 16:53:39 +0300

    Include Sunset Vibes in moon loading screen

--- src/MoonLoader.tsx
+++ src/MoonLoader.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Brand } from "./ui";
+import { EventPartners } from "./ui";
 import "./moon-loader.css";
 
 export function MoonLoader({
@@ -32,7 +32,7 @@ export function MoonLoader({
     >
       <div className="loader-clouds loader-clouds-back" aria-hidden="true" />
       <div className="loader-brand" inert aria-hidden="true">
-        <Brand />
+        <EventPartners />
       </div>
       <div className="loader-cosmos" aria-hidden="true">
         <div className="loader-halo" />
